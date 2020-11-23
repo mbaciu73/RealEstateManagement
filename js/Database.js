862c169cd820e978452f42389c1e703633579d5a
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-11-23 20:51:48 +0000

    commit

--- js/Database.js
+++ js/Database.js
@@ -1,6 +1,8 @@
 const sqlite3 = require('sqlite3');
-const db = sqlite3.Database('assignment.db');
-
-
-
-
+const db = new sqlite3.Database('assignment.db',(err)=>{
+    if(err){
+        console.log(err.message);
+    }
+    
+    console.log('Connected');
+});
