8b89226d4a9ea37670963749d44db3e8b17ab28b
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-11-28 12:22:40 +0000

    commit

--- js/Database.js
+++ js/Database.js
@@ -3,6 +3,29 @@ const db = new sqlite3.Database('assignment.db',(err)=>{
     if(err){
         console.log(err.message);
     }
-    
-    console.log('Connected');
+
+
+})
+const express = require('express');
+const Database = express();
+const bodyparser = require('body-parser');
+const req = require("express");
+const { body, validationResult } = require('express-validator');
+
+Database.use(bodyParser.urlencoded({ extended: true }));
+Database.use(express.static(__dirname + "/public"));
+
+
+
+
+Database.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signin.html");
+});
+
+Database.post('/checkuser',function(req,res){
+    const email = req.body.EmailLogin;
+    const roleid = 'client';
+
+
+    const insert = db.prepare('Insert into User values() ')
 });
