b474c73057f8ab9917b78a4fe950198da92a5f01
C18350801 <55544146+C18350801@users.noreply.github.com>
2020-12-04 20:19:36 +0000

    04.12.2020

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 //const bodyParser = require('body-parser');
 
+const passport = require('passport');
+const bcrypt = require("bcrypt");
+const LocalStrategy = require('passport-local').Strategy;
+
 // new database
 const db = new sqlite3.Database('RealEstate.db');
 
@@ -13,12 +17,52 @@ const findAllCounties = 'SELECT countid, countyname FROM county ORDER BY countyn
 const findAllAreas = 'SELECT areaid, areaname, countid FROM area ORDER BY countid;';
 const findAllCategories = 'SELECT catid, catname FROM procat ORDER BY catid ASC;';
 const findAllTypes = 'SELECT ptypeid, ptypename, catid FROM protype;';
-const findAllProperties = 'SELECT pid, paddr,ptypename,no_bed,no_baths, imgname, areaname from property join pimages using (pid) join area using (areaid) join protype USING (ptypeid) JOIN procat USING (catid) ORDER by pid DESC;';
+const findAllProperties = 'SELECT pid, paddr,ptypename,no_bed,no_baths,imgname,areaname,catname from property join pimages using (pid) join area using (areaid) join protype USING (ptypeid) JOIN procat USING (catid) ORDER by pid DESC;';
 const latestResidentials = 'SELECT pid, paddr,ptypename,no_bed,no_baths, imgname, areaname from property join pimages using (pid) join area using (areaid) join protype USING (ptypeid) JOIN procat USING (catid) where catid != 2 ORDER by pid DESC LIMIT 3;';
 const allProperties = 'SELECT pid, paddr,ptypename,no_bed,no_baths, imgname, areaname from property join pimages using (pid) join area using (areaid) join protype USING (ptypeid) JOIN procat USING (catid) ORDER by pid ASC;';
-const findUser = 'SELECT * FROM reusers;';
+const findPasswordByEmail = 'SELECT email,password FROM reusers where email = $1;';
+const findUserByEmail = 'SELECT email FROM reusers WHERE email = $1;';
+
+const emailNotFound = 'Email not found';
+const incorrectPassword = 'Incorrect Password';
 // instantiate an object of express
 const app = express();
+
+//create the cookie
+app.use(session({
+    secret: process.env.COOKIE_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+//initialise a passport for username and password
+app.use(passport.initialize());
+app.use(passport.session());
+
+//checks if an email exists and if it does exist checks for the corresponding password
+passport.use(new LocalStrategy(function(username, password, done) {
+    const userQuery = db.prepare(findPasswordByEmail);
+    userQuery.get(username, function(error, row) {
+        if (error) return err // error with query
+        if (!row) return done(null, false, { message: emailNotFound });
+        bcrypt.compare(password, row.password, function(err, res) {
+            if (res == true) done(null, { id: row.password });
+            else return done(null, false, { message: incorrectPassword });
+        });
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    return done(null, email.id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+    const userQuery = db.prepare(findUserByEmail);
+    userQuery.get(id, function(err, row) {
+        if (!row) return done(null, false);
+        return done(null, row);
+    });
+});
 
 // set root directory for all files
 app.use(express.static(__dirname + "/public"));
@@ -31,7 +75,7 @@ app.get("/",function(req,res){
 	res.sendFile(__dirname + "/index.html");
 });
 
-// all links
+// all links to the other html pages
 app.get("/property", function(req, res) {
     res.sendFile(__dirname + "/property.html");
 });
@@ -121,6 +165,19 @@ app.get('/retrieveLatestResidentialProperites', function(req, res) {
             res.status(400).json(error);
         } else {
             res.status(200).json(rows);
+        }
+    });
+});
+//
+app.get('/retrieveUser', function(req, res) {
+    const query = db.prepare(findAllAreas);
+    query.all(function(error, rows) {
+        if (error) {
+            console.log(error);
+            res.status(400).json(error);
+        } else {
+            res.status(200).json(rows);
+
         }
     });
 });
--- public/js/properties.js
+++ public/js/properties.js
@@ -1,13 +1,12 @@
-
-
+//populate query selects
 $(document).ready(function() {
   const post = $.get('/retrieveAllProperties');
-  post.done(populateLatestResidential);
+  post.done(populateAllProperties);
   post.fail(displayErrorPopulate);
 });
 
-
-function populateLatestResidential(rows, status, xhr) {
+//function to populate all the properties
+function populateAllProperties(rows, status, xhr) {
   for (let i = 0; i < rows.length; i++) {
 
       $(`<div class="col-md-4 col-sm-6 col-xs-12 p-3">
@@ -15,7 +14,7 @@ function populateLatestResidential(rows, status, xhr) {
         <div class="p_small_details"> <img class="" src="img/${rows[i].imgname}"/>
           <div class="p-4">
             <h5>${rows[i].paddr}</h5>
-            <p>${rows[i].ptypename}</p>
+            <p>${rows[i].catname} : ${rows[i].ptypename} </p>
             <div class="property-details"> <span class=""><i class="fas fa-bed"></i></span>
               <p>${rows[i].no_bed} Bedrooms</p>
               <span class=""><i class="fas fa-bath"></i></span>
@@ -29,9 +28,11 @@ function populateLatestResidential(rows, status, xhr) {
           </div>
         </div>
       </div>
-    </div>`).appendTo('#latest-properties');
+    </div>`).appendTo('#all-properties');
   }// end for
-}// end populateType
+}// end populateAllProperties
+
+//if populate error
 function displayErrorPopulate() {
-console.log('failed to populate');
+  console.log('failed to populate');
 }
